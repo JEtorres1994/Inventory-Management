9bf0789d28715b0d76583481d1f52998c9760973
Jorge Torres <jorgetorres@Jorges-MBP.lan>
2024-08-16 16:46:41 -0400

    added retry logic to menu options

--- InventoryApp.js
+++ InventoryApp.js
@@ -4,17 +4,20 @@ var inventory = require('./current_Inventory.json');
 var specials = require('./specials.json');
 var validator = require('validator');
 var date = new Date();
-
 const day = date.getDay();
+var name = greet(); 
 
-var name = prompt('What is your name ? ');
-//console.log("validator = ", validator.isAlpha(name))
-    if (!validator.isAlpha(name)){
-        console.log("Invalid, Good bye")
-        process.exit()
+var retry = 2
+
+function greet() {
+    name = prompt('What is your name ? ');
+        if (!validator.isAlpha(name)){
+            console.log("Invalid, Good bye")
+            process.exit()
+        }
+    console.log("Good Day, "+name+ "!");
+    return name;
     }
-console.log("Hello "+name+ ", !");
-
 
 console.log("Today is "+JSON.stringify(specials.Specials[day].Day)+".");
     if ( day == 0|| day == 6 ){
@@ -24,185 +27,113 @@ console.log("Today is "+JSON.stringify(specials.Specials[day].Day)+".");
 console.log("The special of the day is " + JSON.stringify(specials.Specials[day].Protein)+" with the side of "+JSON.stringify(specials.Specials[day].Vegetables)+ " and "+JSON.stringify(specials.Specials[day].Fruit)+ ".");
 
 var sp = prompt("Do you want the special of the day?");
+
     if (sp == "yes"){
             console.log("Thank you "+name+", order have been sent to the kitchen.");
             process.exit()
     }
     else if (sp == "no"){
-            console.log("Build your meal");
-                console.log("Select your protein:  \n"+
+
+console.log("Build your meal");
+
+var proteins = pickProtein()  
+var sides = pickVegetable()
+var fruits = pickFruit()
+    }
+
+function pickProtein() {
+     console.log("Select your protein:  \n"+
                     "1: "  + JSON.stringify(inventory.Food.Protein[0].name)+ "\n"+
                     "2: "  + JSON.stringify(inventory.Food.Protein[1].name)+ "\n"+
                     "3: "  + JSON.stringify(inventory.Food.Protein[2].name)+ "\n"+
                     "4: "  + JSON.stringify(inventory.Food.Protein[3].name)+ "\n"+
                     "5: "  + JSON.stringify(inventory.Food.Protein[4].name))
-                var proteins = ""  ;
+
                 var protein = prompt("> ");
-                    if (protein == 1) {
-                        proteins = JSON.stringify(inventory.Food.Protein[0].name) ;
+                    if (protein == 1 ) {
+                        return  JSON.stringify(inventory.Food.Protein[0].name) ;
                     } else if (protein == 2) {
-                        proteins = JSON.stringify(inventory.Food.Protein[1].name) ;
+                        return JSON.stringify(inventory.Food.Protein[1].name) ;
                     } else if (protein == 3) {
-                        proteins = JSON.stringify(inventory.Food.Protein[2].name) ;
+                        return JSON.stringify(inventory.Food.Protein[2].name) ;
                     } else if (protein == 4) {
-                        proteins = JSON.stringify(inventory.Food.Protein[3].name) ;
+                        return JSON.stringify(inventory.Food.Protein[3].name) ;
                     } else if (protein == 5) {
-                        proteins = JSON.stringify(inventory.Food.Protein[4].name) ;
-                    } else {
-                        console.log("unavailable");
-                    }
+                        return JSON.stringify(inventory.Food.Protein[4].name) ;
+                    } else { 
+                        console.log("Invalid option, please pick a valid option.")
+                        if (retry > 0) {
+                            retry--;
+                            return pickProtein()
+                        }
+                        console.log("Come back when you make up your mind... Good Bye.")
+                        process.exit()
+                    }  
+                
+                } 
+    
 
-                console.log("Select your side: \n" +
-                    "1:" +  JSON.stringify(inventory.Food.Vegetables[0].name)+ "\n"+
-                    "2:" +  JSON.stringify(inventory.Food.Vegetables[1].name)+ "\n"+
-                    "3:" +  JSON.stringify(inventory.Food.Vegetables[2].name)+ "\n"+
-                    "4:" +  JSON.stringify(inventory.Food.Vegetables[3].name)+ "\n"+
-                    "5:" +  JSON.stringify(inventory.Food.Vegetables[4].name)+ "\n"+
-                    "6:" +  JSON.stringify(inventory.Food.Vegetables[5].name)+ "\n"+
-                    "7:" +  JSON.stringify(inventory.Food.Vegetables[6].name))
-                var sides =  "" ;
-                var side = prompt("> ");
-                    if (side == 1) {
-                        sides = JSON.stringify(inventory.Food.Vegetables[0].name) ;
-                    } else if (side == 2) {
-                        sides = JSON.stringify(inventory.Food.Vegetables[1].name) ;
-                    } else if (side == 3) {
-                        sides = JSON.stringify(inventory.Food.Vegetables[2].name) ;
-                    } else if (side == 4) {
-                        sides = JSON.stringify(inventory.Food.Vegetables[3].name) ;
-                    } else if (side == 5) {
-                        sides = JSON.stringify(inventory.Food.Vegetables[4].name) ;
-                    } else if (side == 6) {
-                        sides = JSON.stringify(inventory.Food.Vegetables[3].name) ;
-                    } else if (side == 7) {
-                        sides = JSON.stringify(inventory.Food.Vegetables[4].name) ;
-                    } else {
-                        console.log("unavailable");
-                    }
+function pickVegetable() {
+    console.log("Select your protein:  \n"+
+                   "1: "  + JSON.stringify(inventory.Food.Vegetables[0].name)+ "\n"+
+                   "2: "  + JSON.stringify(inventory.Food.Vegetables[1].name)+ "\n"+
+                   "3: "  + JSON.stringify(inventory.Food.Vegetables[2].name)+ "\n"+
+                   "4: "  + JSON.stringify(inventory.Food.Vegetables[3].name)+ "\n"+
+                   "5: "  + JSON.stringify(inventory.Food.Vegetables[4].name))
 
-                console.log("Select a side of fruit: \n" +
-                    "1:" +  JSON.stringify(inventory.Food.Fruit[0].name)+ "\n"+
-                    "2:" +  JSON.stringify(inventory.Food.Fruit[1].name)+ "\n"+
-                    "3:" +  JSON.stringify(inventory.Food.Fruit[2].name)+ "\n"+
-                    "4:" +  JSON.stringify(inventory.Food.Fruit[3].name)+ "\n"+
-                    "5:" +  JSON.stringify(inventory.Food.Fruit[4].name))
-                var fruits = "" ;
-                var fruit = prompt("> ");
-                    if (fruit == 1) {
-                        fruits =  JSON.stringify(inventory.Food.Fruit[0].name);
-                    } else if (fruit == 2) {
-                        fruits =  JSON.stringify(inventory.Food.Fruit[1].name);
-                    } else if (fruit == 3) {
-                        fruits =  JSON.stringify(inventory.Food.Fruit[2].name);
-                    } else if (fruit == 4) {
-                        fruits =  JSON.stringify(inventory.Food.Fruit[3].name);
-                    } else if (fruit == 5) {
-                        fruits =  JSON.stringify(inventory.Food.Fruit[4].name);
-                    } else {
-                        console.log("unavailable");
-                    }          
-        console.log("To confirm your order is "+proteins+", with a side of "+sides+" and "+fruits+" ." )
-    } else {
-        console.log("Invalid Option.")
-    }
+               var vegetable = prompt("> ");
+                   if (vegetable == 1 ) {
+                       return  JSON.stringify(inventory.Food.Vegetables[0].name) ;
+                   } else if (vegetable == 2) {
+                       return JSON.stringify(inventory.Food.Vegetables[1].name) ;
+                   } else if (vegetable == 3) {
+                       return JSON.stringify(inventory.Food.Vegetables[2].name) ;
+                   } else if (vegetable == 4) {
+                       return JSON.stringify(inventory.Food.Vegetables[3].name) ;
+                   } else if (vegetable == 5) {
+                       return JSON.stringify(inventory.Food.Vegetables[4].name) ;
+                   } else { 
+                       console.log("Invalid option, please pick a valid option.")
+                       if (retry > 0) {
+                           retry--;
+                           return pickVegetable()
+                       }
+                       console.log("Come back when you make up your mind... Good Bye.")
+                       process.exit()
+                   }  
+               
+               } 
 
+function pickFruit() {
+    console.log("Select your protein:  \n"+
+                   "1: "  + JSON.stringify(inventory.Food.Fruit[0].name)+ "\n"+
+                   "2: "  + JSON.stringify(inventory.Food.Fruit[1].name)+ "\n"+
+                   "3: "  + JSON.stringify(inventory.Food.Fruit[2].name)+ "\n"+
+                   "4: "  + JSON.stringify(inventory.Food.Fruit[3].name)+ "\n"+
+                   "5: "  + JSON.stringify(inventory.Food.Fruit[4].name))
 
-  //var protein = prompt("Select your protein:  \n" ,
-
-
-
-
-
-
- /*   var protein = prompt("Select your protein:  \n" ,
-        "1" + JSON.stringify(inventory.Food.Protein[0].name)+
-        "2" + JSON.stringify(inventory.Food.Protein[1].name)+ 
-        "3" + JSON.stringify(inventory.Food.Protein[2].name)+ 
-        "4" + JSON.stringify(inventory.Food.Protein[3].name)+ 
-        "5" + JSON.stringify(inventory.Food.Protein[4].name));
-                                                    
-     var protein =("");
-  
-
-
-
-   
-
-
-//console.log("here is today specials " + JSON.stringify(inventory.Food.Protein[0].name))
-
-
-
-
-
-/*
-console.log("setect your next day off: \n"+
-                  "1: Monday \n"+
-                "2: Tuesday \n"+
-                "3: Wednesday \n"+
-                "4: Thursday \n"+
-                "5: Friday\n"+
-                "6: Saturday\n"+
-                "7: Sunday")
-
-var day = "";
-
-var date = prompt("> ");
-
-if (date == 1) {
-   day = "Monday";
-} else if (date == 2) {
-    day = ("Tuesday");
-} else if (date == 3) {
-    day =("Wednesday");
-} else if (date == 4) {
-    day = ("Thursday");
-} else if (date == 5) {
-    day = ("Friday");
-} else if (date == 6) {
-    day = ("Saturday");
-} else if (date == 7) {
-    day = ("Sunday");
-} else {
-    console.log("unavailable");
-}
-
-console.log("Select your favorite musical from this list: \n"+
-    "1: Mama Mia \n"+
-    "2: Beetlejuice \n"+
-    "3: Moulin Rouge \n"+
-    "4: Hamilton \n"+
-    "5: Cabaret \n"+
-    "6: The Lion King\n"+
-    "7: Hairspray")
-
-var event = "";
-
-var event = prompt("> ");
-
-if (event == 1) {
-    event = "Mama Mia";
- } else if (event == 2) {
-    event = ("Beetlejuice");
- } else if (event == 3) {
-    event = ("Moulin Rouge");
- } else if (event == 4) {
-    event = ("Hamilton");
- } else if (event == 5) {
-    event = ("Cabaret");
- } else if (event == 6) {
-    event = ("The Lion King");
- } else if (event == 7) {
-    event = ("Hairspray");
- } else {
-    event = ("The book of Mormon");
-    }
-
-console.log(" "+name+ " will be attending the show of " +event+ " the night of " +day+ " ")
-
-
-
-djfj
-
-*/
-
+               var fruit = prompt("> ");
+                   if (fruit == 1 ) {
+                       return  JSON.stringify(inventory.Food.Fruit[0].name) ;
+                   } else if (fruit == 2) {
+                       return JSON.stringify(inventory.Food.Fruit[1].name) ;
+                   } else if (fruit == 3) {
+                       return JSON.stringify(inventory.Food.Fruit[2].name) ;
+                   } else if (fruit == 4) {
+                       return JSON.stringify(inventory.Food.Fruit[3].name) ;
+                   } else if (fruit == 5) {
+                       return JSON.stringify(inventory.Food.Fruit[4].name) ;
+                   } else { 
+                       console.log("Invalid option, please pick a valid option.")
+                       if (retry > 0) {
+                           retry--;
+                           return pickFruit()
+                       }
+                       console.log("Come back when you make up your mind... Good Bye.")
+                       process.exit()
+                   }  
+               
+               } 
+        
+console.log("To confirm your order is "+proteins+", with a side of "+sides+" and "+fruits+" ." )
+    
